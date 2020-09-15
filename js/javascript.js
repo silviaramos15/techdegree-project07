90aea99b2742098c68b9853996422b0394a2e21f
Silvia <sramos.exp@gmail.com>
2020-09-15 09:44:44 +0100

    first graphic

--- js/javascript.js
+++ js/javascript.js
@@ -14,3 +14,55 @@ $('.menu-toggle').click(function(){
     $(this).toggleClass('open');
   })
   
+  //ALERT BANNER
+  const alertBanner = document.getElementById('alert');
+        //   Create the html for the banner:
+  alertBanner.innerHTML = `<div class="alert-banner">
+                        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+                        <p class="alert-banner-close">x</p>
+                        </div>`;
+        // Add a click event listener to the alert banner:
+  alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = 'none';
+    }
+  });
+
+  //CHART WIDGETS
+        //Line graph
+  let trafficCanvas = document.getElementById('traffic-chart');
+  let trafficData = {
+      labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+      datasets: [{
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: 'rbga(255, 178, 38 .3)',
+      borderWidth: 1,
+    }]
+  };
+        //Object literal to set the iptions to be changed for the chart
+    let trafficOptions = {
+        aspectRatio: 2.5,
+        animation: {
+            duration: 0
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
+    };
+
+        //Create chart itself
+    // const canvas = document.getElementsByClassName('canvas');
+    const canvas_traffic = document.getElementsByClassName('canvas')[0];
+    let trafficChart = new Chart (trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+    });
